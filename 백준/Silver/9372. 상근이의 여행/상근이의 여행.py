c0f7bfd8910be2e9097c41d5a4800d547f78f080
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-03-20 15:34:38 +0900

    [Silver IV] Title: 상근이의 여행, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+TC = int(input())
+
+for _ in range(TC):
+          n, m = map(int, input().split())
+
+          for _ in range(m):
+                  input()
+          
+          print(n-1)
