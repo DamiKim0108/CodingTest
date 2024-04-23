c43fc2cbf0c89172e525c8ee0d212237708d1642
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-04-23 14:55:15 +0900

    [Silver III] Title: 수리공 항승, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,13 @@
+N, L = map(int, input().split())
+pipe = list(map(int, input().split()))
+pipe.sort()
+start = pipe[0]
+count = 1
+
+for i in pipe[1:]:
+          if (i+0.5) - (start-0.5) <= L:
+                  continue
+          else:
+                  start = i
+                  count += 1
+print(count)
