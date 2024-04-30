e2fc8dd0ec5c9615d972d5ccad035ca998f16f06
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-04-30 22:49:17 +0900

    [Gold V] Title: 벼락치기, Time: 592 ms, Memory: 53240 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, T = map(int, input().rsplit())
+
+times, scores = [0], [0]
+
+for _ in range(N):
+    t, s = map(int, input().rsplit())
+    times.append(t)
+    scores.append(s)
+
+dp = [[0 for _ in range(T+1)] for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, T+1):
+        if j >= times[i]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-times[i]] + scores[i])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][T])
