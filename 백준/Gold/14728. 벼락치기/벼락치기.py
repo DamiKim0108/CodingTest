7906ff3b2491a4654e5241bf2c92b0b5a146f2b2
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-05-01 13:10:56 +0900

    [Gold V] Title: 벼락치기, Time: 560 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,21 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-N, T = map(int, input().rsplit())
+N, T = map(int, input().split())
 
-times, scores = [0], [0]
+time, score = [0], [0]
 
 for _ in range(N):
-    t, s = map(int, input().rsplit())
-    times.append(t)
-    scores.append(s)
+          t, s = map(int, input().split())
+          time.append(t)
+          score.append(s)
 
-dp = [[0 for _ in range(T+1)] for _ in range(N+1)]
+dp = [[0]*(T+1) for _ in range(N+1)]
+
+# i개의 단원 중 j시간 공부했을 때 얻을 수 있는 최대 점수
+# j가 현재 과목인 i를 공부할 때 드는 시간보다 크거나 같으면
+# 현재 과목 i를 공부 할 시간이 있는 것임으로
+# 현재 과목 공부를 안했을 때와 현재 과목을 공부했을때의 시간을 비교한다
+# 현재 과목을 공부하지 않았을 때의 최대 점수와 현재 과목을 공부하기 전의 점수 + 현재 과목의 점수 비교
+
 for i in range(1, N+1):
-    for j in range(1, T+1):
-        if j >= times[i]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-times[i]] + scores[i])
-        else:
-            dp[i][j] = dp[i-1][j]
-
+        for j in range(1, T+1):
+                if j>=time[i]:
+                        dp[i][j] = max(dp[i-1][j], dp[i-1][j-time[i]]+score[i])
+                else:
+                        dp[i][j] = dp[i-1][j]
 print(dp[N][T])
