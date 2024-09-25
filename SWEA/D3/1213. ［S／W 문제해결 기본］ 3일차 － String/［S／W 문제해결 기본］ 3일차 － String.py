75bf7a732df975cef74a6611a254a3d230964fc1
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-09-25 17:47:19 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 131 ms, Memory: 46,808 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,10 +1,11 @@
-T = 10
+for tc in range(1, 11):
+    N = int(input())
+    find = input()
+    texts = input()
 
-for tc in range(1, T+1):
-    Tc = int(input())
-    search = input()
-    string = input()
-    
-    ans = string.count(search)
-    
-    print(f'#{tc} {ans}')
+    cnt = 0
+    for i in range(0, len(texts)-1):
+        if texts[i] == find[0]:
+            if texts[i:i+len(find)] == find:
+                cnt += 1
+    print('#{} {}'.format(tc, cnt))
