ff5597cff5b5d34a3899e74b23b2a2758ad969bd
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-09-26 15:34:34 +0900

    [D3] Title: 최장 경로, Time: 136 ms, Memory: 49,108 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,36 +1,28 @@
-def dfs(v, count):
-    global max_len
-    
-    # v 노드 방문처리, v 노드와 연결된 점들 중 방문 안한 노드 방문
-    visited[v] = 1
-    
-    for i in graph[v]:
-        if visited[i] == 0:
-            dfs(i, count+1)
-            
-    # 방문할 수 있는 점이 없다면 방문처리 취소 -> 다른 경로에서 지나갈 수 있음        
-    visited[v] = 0
-    
-    if count > max_len:
-        max_len = count
-
-
 T = int(input())
-
-for tc in range(1, T+1):
-    N, M = map(int, input().split())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+def dfs(x, cnt):
+    global result
+    visited[x] = 1
+    for i in node[x]:
+        if visited[i]==0:
+            visited[i] = 1
+            dfs(i, cnt +1)
+    visited[x] = 0
     
-    graph = [[] for _ in range(N+1)]
-    visited = [0] * (N+1)
+    if cnt > result:
+        result = cnt
+        
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split())
+    node = [[] for i in range(N+1)]
+    result = 0
     
     for i in range(M):
         x, y = map(int, input().split())
-        graph[x].append(y)
-        graph[y].append(x)
+        node[x].append(y)
+        node[y].append(x)
         
-    count, max_len = 0, 0
-    
     for i in range(1, N+1):
-        dfs(i, 1)
-        
-    print(f'#{tc} {max_len}')
+        visited = [0 for _ in range(N+1)]
+        dfs(i,1)
+    print(f'#{test_case} {result}')
