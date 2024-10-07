995ca30ef9496ea7d2d06dfc9ad4d94db32b0d37
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2024-10-07 12:23:34 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 167 ms, Memory: 48,916 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -2,14 +2,11 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    lst = list(map(int, input().split()))
-    
-    # dp 사용 (for문 i, 0~i / lst[i]>lst[j] 면 dp[i], dp[j]+1 중 max)
+    arr = list(map(int, input().split()))
     dp = [1] * N
-    
+
     for i in range(1, N):
         for j in range(i):
-            if lst[i] > lst[j]:
+            if arr[j] <= arr[i]:
                 dp[i] = max(dp[i], dp[j]+1)
-                
-    print(f'#{tc} {max(dp)}')
+    print('#{} {}'.format(tc, max(dp)))
