28122287c23ffae21861efe48876c354ffc767c3
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2025-04-29 16:24:36 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 98 ms, Memory: 60,800 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,24 +1,30 @@
 def dfs(cnt):
     global answer
-    if cnt == t:	
-        temp = ''.join(num)	
-        answer = max(answer, temp)	
+    
+    if cnt == count:
+        temp = int(''.join(num))
+        answer = max(answer, temp)
         return
+    
     for i in range(len(num)):
-        for j in range(i+1, len(num)):
+        for j in range(i+1,len(num)):
             num[i], num[j] = num[j], num[i]
             temp = ''.join(num)
-            if visited.get((temp, cnt), 1):
+            
+            if (temp, cnt) not in visited:
                 visited[(temp, cnt)] = 0
                 dfs(cnt+1)
-            num[i], num[j] = num[j], num[i]	
-            
+                # 끝까지 돌았다면 다시 원상복구 
+            num[i], num[j] = num[j], num[i]
+                
+
 T = int(input())
-for test_case in range(1, T + 1):
-    num, t = input().split()
-    num = list(num)
-    t = int(t)	
-    answer = "0"	
-    visited = {}	
+
+for tc in range(1, T+1):
+    num, count = map(int, input().split())
+    num = list(str(num))
+    answer = 0
+    
+    visited = {}
     dfs(0)
-    print("#{} {}".format(test_case, answer))
+    print("#{} {}".format(tc, answer))
