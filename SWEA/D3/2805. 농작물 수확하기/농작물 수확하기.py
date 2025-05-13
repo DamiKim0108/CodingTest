95c603e55320b50c65a59a7a69d0031b5c2902ad
Dami Kim <84308922+DamiKim0108@users.noreply.github.com>
2025-05-13 16:18:02 +0900

    [D3] Title: 농작물 수확하기, Time: 118 ms, Memory: 60,928 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,20 +1,15 @@
-t = int(input())
+T = int(input())
 
-for tc in range(1, t + 1) :
-    n = int(input())
-    data = [list(map(int, input())) for _ in range(n)]
-    start, end = n // 2, n // 2
+for tc in range(1, T+1):
+    N = int(input())
+    farm = [list(map(int, input())) for _ in range(Ｎ)]
 
+    mid = N//2
     result = 0
-    for i in range(n) :
-        for j in range(start, end + 1) :
-            result += data[i][j]
 
-        if i < n // 2 :
-            start -= 1
-            end += 1
-        else :
-            start += 1
-            end -= 1
-
-    print('#%d %d' % (tc, result))
+    for i in range(N):
+        if i <= mid:
+            result += sum(farm[i][mid-i:mid+i+1])
+        else:
+            result += sum(farm[i][i-mid:N-(i-mid)])
+    print("#{} {}".format(tc, result))
